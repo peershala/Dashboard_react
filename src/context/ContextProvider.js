08f78d8391bfeab94831942079e299bdddb8cb82
Vidushi Pandey <91135324+MuggleHead@users.noreply.github.com>
2023-03-04 19:57:57 +0530

    All files

---
+++ src/context/ContextProvider.js
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+
+    const [thememode, setThememode] = useState(true)
+
+
+    const toggleTheme = ()=>{
+        thememode === false ? setThememode(true) : setThememode(false)
+
+    }
+
+  return (
+    <AppContext.Provider
+      value={{
+        thememode,setThememode,toggleTheme
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const AppState = () => {
+  return useContext(AppContext);
+};
+
+export default AppProvider;
