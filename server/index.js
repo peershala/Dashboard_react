9a6c6a6e925986a0b71c781038000e144717b125
realvineeths <vineeth14shivu@gmail.com>
2023-03-26 09:54:25 +0530

    added env

--- server/index.js
+++ server/index.js
@@ -36,10 +36,10 @@ app.use(bodyParser.json())
 // })
 
 const db = mysql.createConnection({
-    // host:process.env.HOST,
-    // user:process.env.MYSQL_USER,
-    // password:process.env.PASSWORD,
-    // database:process.env.DATABASE
+    host:process.env.HOST,
+    user:process.env.MYSQL_USER,
+    password:process.env.PASSWORD,
+    database:process.env.DATABASE
 })//fill it up
 
 db.connect(function(err) {
